69f1b18ec48587105c67004a31ebccdcd3d6fbea
Demian Wassermann <demian@bwh.harvard.edu>
2013-06-12 11:17:10 -0400

    ENH: Fixed the gradient on the correlation metric and documented the class

--- registration/metric/densities.py
+++ registration/metric/densities.py
@@ -19,10 +19,6 @@ class SquaredDifference(Metric):
         self.tensors = None
         self.vectors = None
 
-    def metric_gradient_transform_parameters(self, parameter):
-        f, grad = self.metric_jacobian_transform_parameters(parameter)
-        return f, numpy.asfortranarray(grad.mean(0))
-
     def metric_gradient(self, points_moving, vectors=None, tensors=None, use_transform=False):
         if use_transform and self.transform is not None:
             points_moving = self.transform.transform_points(points_moving)
@@ -42,6 +38,22 @@ class SquaredDifference(Metric):
 
 
 class Correlation(Metric):
+    r"""
+    Fixed variance correlation metric:
+    Let :math:`Y\in \Re^{N\times 3}` be the fixed set of points and :math:`X\in\Re^{M\times 3}` the
+    moving set of points. Then the metric is
+
+    .. math::
+        m(X, Y) = -\frac{\langle X, Y \rangle^2} {\|X\|^2\|Y\|^2} \propto -\frac{\langle X, Y \rangle^2} {\|X\|^2}
+
+    where
+
+    .. math::
+        \langle X, Y\rangle =  \frac 1 {2\pi\sigma^2}\sum_{ij} \int
+        e^{-.5 \left(\frac {X_i-\eta} \sigma\right)^2}  e^{-.5 \left(\frac {Y_j-\eta} \sigma\right)^2} d\eta
+
+        \|X\|^2 = \langle X, X \rangle
+    """
     def __init__(self,  points_moving, points_fixed, sigma, transform=None):
         self.points_fixed = numpy.require(points_fixed, requirements='C', dtype=numpy.float64)
         self.sigma = sigma
@@ -53,36 +65,6 @@ class Correlation(Metric):
         else:
             self.points_moving = None
 
-    def metric_gradient_transform_parameters(self, parameter):
-        f, grad = self.metric_jacobian_transform_parameters(parameter)
-        return f, numpy.asfortranarray(grad.mean(0))
-
-    def metric_jacobian_transform_parameters(self, parameter):
-        if self.transform is None or self.points_moving is None:
-            return ValueError('transform and points_moving must be set for this')
-
-        self.transform.parameter = parameter
-        points_moving = numpy.ascontiguousarray(self.transform.transform_points(self.points_moving))
-
-        fg, value_gf, _, grad_gf = gauss_transform(
-            points_moving, self.points_fixed, self.sigma
-        )
-
-        moving_norm, value_gg, _, _ = gauss_transform(
-            points_moving, points_moving, self.sigma
-        )
-
-        fg_sqr = fg ** 2
-
-        f = -fg_sqr / (self.fixed_norm * moving_norm)
-
-        grad = -(2 * fg / (self.fixed_norm * moving_norm) * (value_gg - fg / moving_norm * value_gf) * grad_gf)
-
-        jacobian = self.transform.jacobian(points_moving)
-        grad = numpy.asfortranarray((jacobian * grad[:, None, :]).sum(-1))
-
-        return f, grad
-
     def metric_gradient(self, points_moving, vectors=None, tensors=None, use_transform=False):
         if use_transform and self.transform is not None:
             points_moving = self.transform.transform_points(points_moving)
@@ -91,15 +73,15 @@ class Correlation(Metric):
             points_moving, self.points_fixed, self.sigma
         )
 
-        moving_norm, value_gg, _, _ = gauss_transform(
+        moving_norm, value_gg, _, grad_gg = gauss_transform(
             points_moving, points_moving, self.sigma
         )
 
-        fg_sqr = fg ** 2
-
-        f = -fg_sqr / (self.fixed_norm * moving_norm)
-
-        grad = (2 * fg / (self.fixed_norm * moving_norm) * (value_gg - fg / moving_norm * value_gf) * grad_gf)
+        f = -fg * fg / moving_norm
+        grad = 2 * fg / moving_norm * (
+            grad_gg * (fg / moving_norm) -
+            grad_gf
+        )
 
         return f, grad
 
